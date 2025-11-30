a3bec5dc114c9a1b8df63904d444de2f611d6c16
qajohn121 <qajohn121@gmail.com>
2025-11-29 21:57:34 -0600

    Add Quote model

---
+++ lib/models/quote.dart
@@ -0,0 +1,11 @@
+class Quote {
+  final String text;
+  final String category;
+  final String? reference;
+
+  Quote({
+    required this.text,
+    required this.category,
+    this.reference,
+  });
+}
